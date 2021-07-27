98521f7a132a1a62b43bb72eabd389347ed40a34
Nixon Gamboa <nrgamboa97@gmail.com>
2021-07-27 16:31:39 -0500

    Step tree

---
+++ infrastructure/src/main/java/ApplicationMain.java
@@ -0,0 +1,9 @@
+public class ApplicationMain {
+
+    public static void main(String[] args) {
+        BooksService booksService = BooksServiceFactory.getBooksService(new BooksRepositoryImpl());
+        ConsoleApp consoleApp = new ConsoleApp(System.console(), booksService);
+        consoleApp.waitForCommand();
+    }
+
+}
